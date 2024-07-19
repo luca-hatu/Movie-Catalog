838a9d398dc56838ae90a870a75ab478f71c45f2
Luca-Matei HATU <lucamhatu@gmail.com>
2024-07-19 19:59:30 +0200

    resolved the problem of IMDB Id by switch to TMDB

--- script.js
+++ script.js
@@ -5,10 +5,50 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.getElementById('search-input');
     const movieList = document.getElementById('movie-list');
     const trendingList = document.getElementById('trending-list');
-    const topChartsList = document.getElementById('top-charts-list');
     const favoriteList = document.getElementById('favorite-list');
     const watchlistList = document.getElementById('watchlist-list');
-    const apiKey = '57b82531'; 
+    const apiKey = 'cde8a23dec2b79e02c74f516c214114b'; 
+
+    const searchPopup = document.getElementById('search-popup');
+    const favoritesPopup = document.getElementById('favorites-popup');
+    const watchlistPopup = document.getElementById('watchlist-popup');
+    const detailsPopup = document.getElementById('details-popup');
+    const searchPopupClose = document.getElementById('search-popup-close');
+    const favoritesPopupClose = document.getElementById('favorites-popup-close');
+    const watchlistPopupClose = document.getElementById('watchlist-popup-close');
+    const detailsPopupClose = document.getElementById('details-popup-close');
+
+    const searchIcon = document.getElementById('search-icon');
+    const favoritesIcon = document.getElementById('favorites-icon');
+    const watchlistIcon = document.getElementById('watchlist-icon');
+
+    searchIcon.addEventListener('click', () => {
+        searchPopup.style.display = 'flex';
+    });
+
+    favoritesIcon.addEventListener('click', () => {
+        favoritesPopup.style.display = 'flex';
+    });
+
+    watchlistIcon.addEventListener('click', () => {
+        watchlistPopup.style.display = 'flex';
+    });
+
+    searchPopupClose.addEventListener('click', () => {
+        searchPopup.style.display = 'none';
+    });
+
+    favoritesPopupClose.addEventListener('click', () => {
+        favoritesPopup.style.display = 'none';
+    });
+
+    watchlistPopupClose.addEventListener('click', () => {
+        watchlistPopup.style.display = 'none';
+    });
+
+    detailsPopupClose.addEventListener('click', () => {
+        detailsPopup.style.display = 'none';
+    });
 
     searchButton.addEventListener('click', () => {
         const query = searchInput.value;
@@ -19,53 +59,23 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     async function fetchMovies(query) {
-        const url = `http://www.omdbapi.com/?s=${query}&apikey=${apiKey}`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
         try {
             const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
             const data = await response.json();
-            if (data.Response === "True") {
-                displayMovies(data.Search, movieList);
+            console.log('Fetched movies:', data);
+            if (data.results.length > 0) {
+                displayMovies(data.results, movieList);
             } else {
-                console.log(`Error: ${data.Error}`);
-                movieList.innerHTML = `<p>${data.Error}</p>`;
+                console.log('No movies found');
+                movieList.innerHTML = `<p>No movies found</p>`;
             }
         } catch (error) {
             console.error("Error fetching data: ", error);
-        }
-    }
-
-    async function fetchTrendingMovies() {
-        const currentYear = new Date().getFullYear();
-        const url = `http://www.omdbapi.com/?s=&y=${currentYear}&apikey=${apiKey}`;
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            if (data.Response === "True") {
-                const sortedMovies = data.Search.sort((a, b) => b.Year - a.Year);
-                displayMovies(sortedMovies, trendingList);
-            } else {
-                console.log(`Error: ${data.Error}`);
-                trendingList.innerHTML = `<p>${data.Error}</p>`;
-            }
-        } catch (error) {
-            console.error("Error fetching trending movies: ", error);
-        }
-    }
-
-    async function fetchTopChartsMovies() {
-        const url = `http://www.omdbapi.com/?s=&apikey=${apiKey}`;
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            if (data.Response === "True") {
-                const sortedMovies = data.Search.sort((a, b) => b.imdbRating - a.imdbRating);
-                displayMovies(sortedMovies.slice(0, 10), topChartsList);
-            } else {
-                console.log(`Error: ${data.Error}`);
-                topChartsList.innerHTML = `<p>${data.Error}</p>`;
-            }
-        } catch (error) {
-            console.error("Error fetching top charts movies: ", error);
+            movieList.innerHTML = `<p>Error fetching data</p>`;
         }
     }
 
@@ -75,41 +85,45 @@ document.addEventListener('DOMContentLoaded', () => {
             const movieItem = document.createElement('div');
             movieItem.classList.add('movie-item');
             movieItem.innerHTML = `
-                <img src="${movie.Poster}" alt="${movie.Title}">
-                <h3>${movie.Title}</h3>
-                <p>${movie.Year}</p>
-                <div class="ratings">IMDB Rating: ${movie.imdbRating || 'N/A'}</div>
-                <span class="material-icons favorite-button">${isFavorite(movie.imdbID) ? 'favorite' : 'favorite_border'}</span>
-                <span class="material-icons watchlist-button">${isInWatchlist(movie.imdbID) ? 'bookmark' : 'bookmark_border'}</span>
+                <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+                <h3>${movie.title}</h3>
+                <p class="ratings">Rating: ${movie.vote_average}</p>
+                <button class="details-button" data-movie-id="${movie.id}">Details</button>
+                <span class="material-icons favorite-button" data-movie-id="${movie.id}">favorite_border</span>
+                <span class="material-icons watchlist-button" data-movie-id="${movie.id}">bookmark_border</span>
             `;
-            const favoriteButton = movieItem.querySelector('.favorite-button');
-            favoriteButton.addEventListener('click', (event) => {
-                event.stopPropagation();
-                toggleFavorite(movie, favoriteButton);
-            });
-            const watchlistButton = movieItem.querySelector('.watchlist-button');
-            watchlistButton.addEventListener('click', (event) => {
-                event.stopPropagation();
-                toggleWatchlist(movie, watchlistButton);
-            });
-            movieItem.addEventListener('click', () => {
-                fetchMovieDetails(movie.imdbID);
-            });
             container.appendChild(movieItem);
+
+            const detailsButton = movieItem.querySelector('.details-button');
+            const favoriteButton = movieItem.querySelector('.favorite-button');
+            const watchlistButton = movieItem.querySelector('.watchlist-button');
+
+            detailsButton.addEventListener('click', () => {
+                showDetails(movie.id);
+            });
+
+            favoriteButton.addEventListener('click', () => {
+                addToFavorites(movie);
+                favoriteButton.textContent = 'favorite';
+            });
+
+            watchlistButton.addEventListener('click', () => {
+                addToWatchlist(movie);
+                watchlistButton.textContent = 'bookmark';
+            });
         });
     }
 
-    async function fetchMovieDetails(imdbID) {
-        const url = `http://www.omdbapi.com/?i=${imdbID}&apikey=${apiKey}`;
+    async function showDetails(movieId) {
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
         try {
             const response = await fetch(url);
-            const data = await response.json();
-            if (data.Response === "True") {
-                displayMovieDetails(data);
-            } else {
-                console.log(`Error: ${data.Error}`);
-                document.getElementById('movie-details').innerHTML = `<p>${data.Error}</p>`;
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
             }
+            const movie = await response.json();
+            console.log('Fetched movie details:', movie);
+            displayMovieDetails(movie);
         } catch (error) {
             console.error("Error fetching movie details: ", error);
         }
@@ -118,69 +132,87 @@ document.addEventListener('DOMContentLoaded', () => {
     function displayMovieDetails(movie) {
         const movieDetails = document.getElementById('movie-details');
         movieDetails.innerHTML = `
-            <img src="${movie.Poster}" alt="${movie.Title}">
-            <h3>${movie.Title}</h3>
-            <p><strong>Year:</strong> ${movie.Year}</p>
-            <p><strong>Genre:</strong> ${movie.Genre}</p>
-            <p><strong>Director:</strong> ${movie.Director}</p>
-            <p><strong>Plot:</strong> ${movie.Plot}</p>
-            <p><strong>IMDB Rating:</strong> ${movie.imdbRating || 'N/A'}</p>
+            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+            <p>${movie.overview}</p>
+            <p>Rating: ${movie.vote_average}</p>
+            <p>Release Date: ${movie.release_date}</p>
         `;
-        document.getElementById('results').style.display = 'none';
-        document.getElementById('details').style.display = 'block';
+        detailsPopup.style.display = 'flex';
     }
 
-    function toggleFavorite(movie, button) {
+    function addToFavorites(movie) {
         let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-        const movieIndex = favorites.findIndex(fav => fav.imdbID === movie.imdbID);
-        if (movieIndex > -1) {
-            favorites.splice(movieIndex, 1);
-            button.textContent = 'favorite_border';
-        } else {
+        if (!favorites.some(fav => fav.id === movie.id)) {
             favorites.push(movie);
-            button.textContent = 'favorite';
+            localStorage.setItem('favorites', JSON.stringify(favorites));
+            displayFavorites();
         }
-        localStorage.setItem('favorites', JSON.stringify(favorites));
-        displayFavorites();
     }
 
-    function toggleWatchlist(movie, button) {
+    function addToWatchlist(movie) {
         let watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
-        const movieIndex = watchlist.findIndex(watch => watch.imdbID === movie.imdbID);
-        if (movieIndex > -1) {
-            watchlist.splice(movieIndex, 1);
-            button.textContent = 'bookmark_border';
-        } else {
+        if (!watchlist.some(watch => watch.id === movie.id)) {
             watchlist.push(movie);
-            button.textContent = 'bookmark';
+            localStorage.setItem('watchlist', JSON.stringify(watchlist));
+            displayWatchlist();
         }
-        localStorage.setItem('watchlist', JSON.stringify(watchlist));
-        displayWatchlist();
-    }
-
-    function isFavorite(movieID) {
-        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-        return favorites.some(fav => fav.imdbID === movieID);
-    }
-
-    function isInWatchlist(movieID) {
-        const watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
-        return watchlist.some(watch => watch.imdbID === movieID);
     }
 
     function displayFavorites() {
         const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-        displayMovies(favorites, favoriteList);
+        favoriteList.innerHTML = '';
+        favorites.forEach(movie => {
+            const movieItem = document.createElement('div');
+            movieItem.classList.add('movie-item');
+            movieItem.innerHTML = `
+                <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+                <h3>${movie.title}</h3>
+                <p>${movie.release_date}</p>
+            `;
+            movieItem.addEventListener('click', () => {
+                showDetails(movie.id);
+            });
+            favoriteList.appendChild(movieItem);
+        });
     }
 
     function displayWatchlist() {
         const watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
-        displayMovies(watchlist, watchlistList);
+        watchlistList.innerHTML = '';
+        watchlist.forEach(movie => {
+            const movieItem = document.createElement('div');
+            movieItem.classList.add('movie-item');
+            movieItem.innerHTML = `
+                <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+                <h3>${movie.title}</h3>
+                <p>${movie.release_date}</p>
+            `;
+            movieItem.addEventListener('click', () => {
+                showDetails(movie.id);
+            });
+            watchlistList.appendChild(movieItem);
+        });
     }
-
-    // Fetch data on load
-    fetchTrendingMovies();
-    fetchTopChartsMovies();
     displayFavorites();
     displayWatchlist();
+
+    async function fetchTrendingMovies() {
+        const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`;
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log('Fetched trending movies:', data);
+            if (data.results.length > 0) {
+                displayMovies(data.results, trendingList);
+            }
+        } catch (error) {
+            console.error("Error fetching trending movies: ", error);
+        }
+    }
+
+    fetchTrendingMovies();
 });
